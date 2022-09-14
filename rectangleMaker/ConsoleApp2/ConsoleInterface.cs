6086bd3553647861dcd45b59ca0eef4043918268
slaykon84 <104734887+slaykon84@users.noreply.github.com>
2022-09-15 00:06:14 +0300

    Add files via upload

---
+++ rectangleMaker/ConsoleApp2/ConsoleInterface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ConsoleApp2
+{
+    class ConsoleInterface
+    {
+        static void Main()
+        {
+            makeQuad(16, 16, '#');
+            Console.ReadKey();
+        }
+        static void makeQuad(int x, int y, char character = '@')
+        {
+            //column configuration
+            string xaxis = string.Empty;
+            for (int i = 0; i < x; i++)
+            {
+                xaxis += character + " ";
+            }
+            //writing to row
+            for (int i = 0; i < y; i++)
+            {
+                Console.WriteLine(xaxis);
+            }
+        }
+    }
+
+}
---
+++ rectangleMaker/ConsoleApp2/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console1
+{
+   
+    public class CustomerInformation
+    {
+        public int Id;
+        public int joinYear;
+        public CustomerInformation(int id , int joinyear)
+        {
+            Id = id;
+            joinYear = joinyear;
+        }
+        
+        public string firstName { get;  set; }
+        public string lastName { get; set; }
+        public int location { get; set; }
+    }
+}
+
